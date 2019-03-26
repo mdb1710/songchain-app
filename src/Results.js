6f30dab52933d8bc78f7a8b0c53ad6483d0a0822
mdb1710 <mdb1710@gmail.com>
2019-03-26 00:26:29 -0400

    fixed 304 request

--- src/Results.js
+++ src/Results.js
@@ -6,28 +6,44 @@ class Results extends Component {
   
     static contextType = SearchContext;
 
-    // updatePlaylists = () => {
-    //     const { mood, genre, playlists } = this.context;
-    //     console.log(playlists);
-    //     this.context.displaySearchResults(mood, genre);
-    // }
+    
+
+    componentDidMount(){
+        const { mood, genre } = this.context
+        this.context.displaySearchResults(mood, genre);
+    }
+
+   
     
     render(){
         const { artist, mood, genre, playlists } = this.context;
-        const test = this.context.displaySearchResults(mood, genre);
-        console.log(playlists);
+        
+        
         return(
         <>    
-          {/* <SearchContext.Consumer playlist={playlists}> */}
+          
             <h3>Here are your {mood} & {genre} search results for {artist} </h3>
             <div className="result-list">
-               <ul>
+                <ul>
                    {playlists.map((note, index) =>{
                        return (
-                           <li key={index}>{note.name}</li>
+                           <li key={index}>
+                             <div className="result-name" id={index}>
+                              <p>Playlist Name: {note.name}</p>
+                             </div>
+                             <div className="result-image">
+                              <p>{note.images[0].url}</p>
+                             </div>
+                             <div className="result-tracks">
+                              <p>Tracks: {note.tracks.total}</p>
+                             </div>
+                             <div className="result-url">
+                             <p>Start Listening Here</p>
+                             </div>
+                           </li>  
                        )
                    })}
-               </ul>
+                </ul>
                <p>See more playlists with artist here</p>
             </div>
             <div className="new-search">
@@ -36,7 +52,7 @@ class Results extends Component {
               </Link>
             </div>
             
-         {/* </SearchContext.Consumer>    */}
+         
         </>
         )
     }
